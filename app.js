d260a97dce9f78f4e8924c602441c329e8297216
Aakaash Jois <aakaashjois@gmail.com>
2017-03-16 00:35:25 +0530

    Completed Project
    Added basic functionality needed to complete project.

---
+++ app.js
@@ -0,0 +1,19 @@
+function getDataFromApi(searchTerm, callback) {
+    $.getJSON("https://www.googleapis.com/youtube/v3/search", {
+        part: "snippet",
+        key: "AIzaSyBt0xUvvQ_roGDtew8dmfiWmzfkxtgVw68",
+        q: searchTerm
+    }, callback);
+}
+
+function makeSearch() {
+    $('.thumbnail-holder').empty();
+    getDataFromApi($('.search').find('#search').val(), extractData);
+}
+
+function extractData(data) {
+    var results = data.items;
+    for (var i = 0; i < results.length; i++)
+        if (results[i].id.kind == "youtube#video")
+            $('.thumbnail-holder').append('<img src=' + results[i].snippet.thumbnails.medium.url + '>');
+}
